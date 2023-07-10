8650303cd13878c8399ef905bb54d7aa8d119d6c
ajjohnson2024 <139026871+ajjohnson2024@users.noreply.github.com>
2023-07-10 02:47:24 -0400

    Created index.js page

--- src/index.js
+++ src/index.js
@@ -1,53 +1,7 @@
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-text-input");
-  let h2 = document.querySelector("h2");
-
-  if (searchInput.value) {
-    h2.innerHTML = `Searching for ${searchInput.value}...`;
-    getWeatherData(searchInput.value);
-  } else {
-    h2.innerHTML = null;
-    alert("Please type a city");
-  }
+function displayTemperture(response) {
+  let apiKey = "9f8of3b322tfa70f5cff8a4aa4a3ab35";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=9f8of3b322tfa70f5cff8a4aa4a3ab35&units=metric`;
 }
 
-function getWeatherData(city) {
-  let apiKey = "66ab130597ee92574b418adad0be78d4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
-  function showTemperature(response) {
-    displayWeatherData(response);
-  }
-
-  axios.get(apiUrl).then(showTemperature).catch(displayError);
-}
-
-function displayWeatherData(response) {
-  let h2 = document.querySelector("h2");
-  let temperature = Math.round(response.data.main.temp);
-  h2.innerHTML = `${response.data.name}, ${response.data.sys.country}: ${temperature}°C, ${response.data.weather[0].description}`;
-}
-
-function displayError(error) {
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `Error: ${error.message}`;
-}
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
-
-function displayDateTime() {
-  const currentDate = new Date();
-  const options = {
-    weekday: "long",
-    hour: "numeric",
-    minute: "numeric",
-    timeZone: "America/New_York",
-  };
-  const formattedDate = currentDate.toLocaleString("en-US", options);
-  document.getElementById("current-time").textContent = formattedDate;
-  console.log("Current date and time:", formattedDate);
-}
-
-displayDateTime();
+console.log(apiUrl);
+axios.get(url).then(displayTemperture);
