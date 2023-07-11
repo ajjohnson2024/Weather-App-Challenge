5c962248603338bad0980724a950e7875e28d1ee
ajjohnson2024 <139026871+ajjohnson2024@users.noreply.github.com>
2023-07-10 22:48:52 -0400

    Updated Api Url

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-let apiKey = "66ab130597ee92574b418adad0be78d4";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let apiKey = "36a2a52539d5ce4b6e400d39a182ca91";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+  city
+)}&appid=${apiKey};`;
 
 console.log(apiUrl);
